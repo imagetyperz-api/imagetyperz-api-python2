8f72b3cd7b1cbf5e25e27f2f301f890052fedfdc
ImagetyperzAPI <imagetyperz@gmail.com>
2018-02-13 08:48:00 +0200

    1st commit

---
+++ imagetyperzapi2/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'imagetyperzapi2-python2'
+__version__ = '1.0.0'
+__author__ = 'Imagetyperz Dev'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 ImageTyperz.com'
+
+from imagetyperzapi import *
