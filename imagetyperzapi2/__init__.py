fb4c4b743d2e4c2c4e21a9414b38585f67806ec4
imagetyperz-api <imagetyperz@gmail.com>
2020-10-02 16:10:23 +0300

    updated lib, examples and readme

--- imagetyperzapi2/__init__.py
+++
@@ -1,7 +0,0 @@
-__title__ = 'imagetyperzapi2-python2'
-__version__ = '1.0.0'
-__author__ = 'Imagetyperz Dev'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2018 ImageTyperz.com'
-
-from imagetyperzapi import *
